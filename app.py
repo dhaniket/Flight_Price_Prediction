eba439af4dceff0f2a92632a7b0620d25388d801
Dhaniket Shambharkar <85407454+dhaniket@users.noreply.github.com>
2022-08-31 06:16:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,180 +1,75 @@
-from flask import Flask, request, render_template
-from flask_cors import cross_origin
-import sklearn
+import streamlit as st
 import pickle
-import pandas as pd
 import numpy as np
 
-app = Flask(__name__)
-model = pickle.load(open("model.pkl", "rb"))
+# import the model
+pipe = pickle.load(open('pipe.pkl','rb'))
+df = pickle.load(open('df.pkl','rb'))
+
+st.title("Flight Price Predictor")
+
+# brand
+airline = st.selectbox('Airline',df['airline'].unique())
+
+# source city
+source_city = st.selectbox('Source',df['source_city'].unique())
+
+# destination city
+destination_city = st.selectbox('Destination',df['destination_city'].unique())
+
+# departure_time
+departure_time = st.selectbox('Departure Time',df['departure_time'].unique())
+
+# arrival_time
+arrival_time = st.selectbox('Arrival Time',df['arrival_time'].unique())
 
 
+# Ram
+ram = st.selectbox('RAM(in GB)',[2,4,6,8,12,16,24,32,64])
 
-@app.route("/")
-@cross_origin()
-def home():
-    return render_template("home.html")
+# weight
+weight = st.number_input('Weight of the Laptop')
 
+# Touchscreen
+touchscreen = st.selectbox('Touchscreen',['No','Yes'])
 
+# IPS
+ips = st.selectbox('IPS',['No','Yes'])
 
+# screen size
+screen_size = st.number_input('Screen Size')
 
-@app.route("/predict", methods = ["GET", "POST"])
-@cross_origin()
-def predict():
-    if request.method == "POST":
+# resolution
+resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
 
-        # Date_of_Journey
-        date_dep = request.form["Dep_Time"]
-        # print("Journey Date : ",Journey_day, Journey_month)
+#cpu
+cpu = st.selectbox('CPU',df['Cpu brand'].unique())
 
-        # Departure
-        
-        Source = request.form['Departure_Time']
-        if(Source == 'Evening'):
-            departure_time=2
-        elif(Source == 'Early_Morning'):
-            departure_time=1
-        
-        elif(Source == 'Morning'):
-            departure_time=4
+hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
 
-        elif(Source == 'Afternoon'):
-            departure_time=0
+ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
 
-        elif(Source == 'Night'):
-            departure_time=5
+gpu = st.selectbox('GPU',df['Gpu brand'].unique())
 
-        elif(Source == 'Late_Night'):
-            departure_time=3
-        # print("Departure : ",Dep_hour, Dep_min)
+os = st.selectbox('OS',df['os'].unique())
 
-        # Arrival
-        date_arr = request.form["Arrival_Time"]
-        Source = request.form['Arrival_Time']
-        if(Source == 'Evening'):
-            arrival_time=2
-        elif(Source == 'Early_Morning'):
-            arrival_time=1
-        
-        elif(Source == 'Morning'):
-            arrival_time=4
+if st.button('Predict Price'):
+    # query
+    ppi = None
+    if touchscreen == 'Yes':
+        touchscreen = 1
+    else:
+        touchscreen = 0
 
-        elif(Source == 'Afternoon'):
-            arrival_time=0
+    if ips == 'Yes':
+        ips = 1
+    else:
+        ips = 0
 
-        elif(Source == 'Night'):
-            arrival_time=5
+    X_res = int(resolution.split('x')[0])
+    Y_res = int(resolution.split('x')[1])
+    ppi = ((X_res**2) + (Y_res**2))**0.5/screen_size
+    query = np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
 
-        elif(Source == 'Late_Night'):
-            arrival_time=3
-
-        # print("Arrival : ", Arrival_hour, Arrival_min)
-        
-        ## Days LEft
-        days_left = request.form['days']
-
-        # Duration
-        duration = request.form['text']
-
-        # print("Duration : ", dur_hour, dur_min)
-
-
-        # Class
-        Source = request.form['Class']
-        if(Source == 'Economy'):
-            sclass=1
-        elif(Source == 'Business'):
-            sclass=0
-
-        # Total Stops
-        stops = int(request.form["Stops"])
-        # print(Total_stops)
-
-        # Airline
-        # AIR ASIA = 0 (not in column)
-        Source=request.form['Source']
-        if(Source=='SpiceJet'):
-            airline=4
-
-        elif (Source=='IndiGo'):
-            airline=3
-
-        elif (Source=='AirAsia'):
-            airline=0
-
-        elif (Source=='Vistara'):
-            airline=5
-
-        elif (Source=='GO_FIRST'):
-            airline=2
-
-        elif (Source=='Air India'):
-            airline=1
-
-        # Source
-        # Banglore = 0 (not in column)
-        Source = request.form["Source"]
-        if (Source == 'Mumbai'):
-            source_city=5
-        
-        elif (Source == 'Delhi'):
-            source_city=2
-
-        elif (Source == 'Bangalore'):
-            source_city=0
-
-        elif (Source == 'Hyderabad'):
-            source_city=3
-
-        elif (Source == 'Kolkata'):
-            source_city=4
-        
-        elif (Source == 'Chennai'):
-            source_city=1
-
-        Source = request.form["Destination"]
-        if (Source == 'Mumbai'):
-            destination_city=5
-        
-        elif (Source == 'Delhi'):
-            destination_city=2
-
-        elif (Source == 'Bangalore'):
-            destination_city=0
-
-        elif (Source == 'Hyderabad'):
-            destination_city=3
-
-        elif (Source == 'Kolkata'):
-            destination_city=4
-        
-        elif (Source == 'Chennai'):
-            destination_city=1
-
-        query = np.array([
-            airline,
-            source_city,
-            departure_time,
-            stops,
-            arrival_time,
-            destination_city,
-            sclass,
-            duration,
-            days_left
-        ])
-        query = query.reshape(1,9)
-
-        prediction = model.predict(query)
-
-        output=round(prediction[0],2)
-
-        return render_template('home.html',prediction_text="Your Flight price is Rs. {}".format(output))
-
-
-    return render_template("home.html")
-
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    query = query.reshape(1,12)
+    st.title("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(query)[0]))))
